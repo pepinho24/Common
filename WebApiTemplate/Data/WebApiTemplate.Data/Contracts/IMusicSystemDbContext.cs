490327d1db04f72b1c0a124c83ec67daa42f8ef3
pepinho24 <p_mil4ev@hotmail.com>
2015-11-07 16:54:52 +0200

    Implemented repository pattern

---
+++ WebApiTemplate/Data/WebApiTemplate.Data/Contracts/IMusicSystemDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApiTemplate.Data.Contracts
+{
+    using System.Data.Entity;
+    using WebApiTemplate.Models;
+    using System.Data.Entity.Infrastructure;
+
+    public interface IWebApiTemplateDbContext
+    {
+      
+        IDbSet<SampleClass> SampleClasses { get; set; }
+        
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        void Dispose();
+
+        int SaveChanges();
+    }
+}
